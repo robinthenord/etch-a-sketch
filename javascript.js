31ec72dfc8351682c42837f235ca60ea092964ec
Robin <168934937+robinthenord@users.noreply.github.com>
2024-05-31 03:17:02 +0200

    add buttons

--- javascript.js
+++ javascript.js
@@ -4,13 +4,13 @@ function createGrid(num) {
   for (i = 0; i < num; i++) {
     const row = document.createElement("div");
     row.className = "row";
-    for (j=0; j < num; j++) {
+    for (j = 0; j < num; j++) {
       const cell = document.createElement("div");
       cell.className = "gridSquare";
-      row.appendChild(cell)
+      row.appendChild(cell);
     }
     gridContainer.appendChild(row);
   }
 }
 
-createGrid(100)
+createGrid(16);
