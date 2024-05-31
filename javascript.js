7f8d78b74d1d5d4449a6cc94ecce77124dafbc37
Robin <168934937+robinthenord@users.noreply.github.com>
2024-05-31 02:55:09 +0200

    set up grid and add function to create grid

--- javascript.js
+++ javascript.js
@@ -1,1 +1,16 @@
-let container = document.querySelector("#container");
+let gridContainer = document.querySelector(".container");
+
+function createGrid(num) {
+  for (i = 0; i < num; i++) {
+    const row = document.createElement("div");
+    row.className = "row";
+    for (j=0; j < num; j++) {
+      const cell = document.createElement("div");
+      cell.className = "gridSquare";
+      row.appendChild(cell)
+    }
+    gridContainer.appendChild(row);
+  }
+}
+
+createGrid(100)
